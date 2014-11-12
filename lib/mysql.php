0bbac047d3ef81c71a7204f495163f4edb7e0c0f
Tim Cassels <cassels2025@gmail.com>
2014-11-12 00:01:36 -0500

    The base of the MySQL library is layed out. Included the GPL-2 header as well. 	modified:   lib/mysql.php

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,24 +1,55 @@
 <?php
+########################################################################
+# One line to give the program's name and a brief idea of what it does.
+# Copyright (C) 2014 Timothy Cassels <cassels2025@gmail.com
+# 
+# This program is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License version 2 as 
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License 
+# along with this program; if not, write to the Free Software 
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#########################################################################
+# Filename: lib/mysql.php
+
 
 namespace Framework\Lib;
 
 
 class MySQL {
-	private $_host;
-	private $_user;
-	private $_pass;
-	private $_db;
-	private $conn;
+	private $_query_result;
 	
-	public function __construct($host, $user, $pass, $db)
+	public function __construct($host, $user, $pass)
 	{
-		$this->_host = $host;
-		$this->_user = $user;
-		$this->_pass = $pass;
-		$this->_db = $db;
 		try {
-			$conn = mysql_connect($host, $user, $pass);
+			mysql_connect($host, $user, $pass);
 		}
+		return $this;
+	}
+	
+	public function select_database($db)
+	{
+		try {
+			mysql_select_db($db);
+		}
+		return $this;
+	}
+	
+	public function query($query)
+	{
+		$this->_query_result = mysql_query($query);
+		return $this;
+	}
+	
+	public function get_assoc()
+	{
+		return mysql_fetch_assoc($this->_query_result);
 	}
 }
 
